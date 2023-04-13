a75bd3dadaad96d4640fc722cd69d2125f0253c9
Ivan Krivosheev <py.krivosheev@gmail.com>
2023-04-13 17:23:04 +0300

    Removed logging configuration

--- asyauth/__init__.py
+++ asyauth/__init__.py
@@ -4,13 +4,3 @@
 #  Tamas Jos (@skelsec)
 #
 
-import logging
-
-logger = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-formatter = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-logger.propagate = False
